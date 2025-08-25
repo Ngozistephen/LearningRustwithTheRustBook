70cf67de29f251afc90ead436dc3438e42484d2d
Stephen Ngozi <ngozi.stephen99@gmail.com>
2025-08-25 15:21:52 +0100

    test practice

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,18 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32,
 }
 
+
+impl Rectangle { 
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
 #[cfg(test)]
-mod tests {
+
+mod  tests{
     use super::*;
 
     #[test]
-    fn exploration() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn larager_can_hold_smaller(){
+        let larger: Rectangle = Rectangle { width: 8, height: 9 };
+        let smaller: Rectangle = Rectangle { width: 5, height: 1 };
+
+        assert!(larger.can_hold(&smaller));
     }
+
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn smaller_cannot_hold_larger(){
+        let larger:Rectangle = Rectangle { width: 8, height: 7 };
+        let smaller:Rectangle = Rectangle { width: 5, height: 1 };
+
+          assert!(!smaller.can_hold(&larger));
     }
+
 }
